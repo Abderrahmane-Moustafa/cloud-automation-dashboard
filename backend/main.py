db444e69e2e0dbd1f7b6cb2e486bdab37754d779
Abderrahmane-Moustafa <abderrahmanmoustafa@yahoo.com>
2025-07-19 10:11:40 +0200

    Restore clean working version from backup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from backend.api import vm, storage, ui, metrics, billing
@@ -18,17 +17,6 @@ app.include_router(storage.router)
 app.include_router(ui.router)
 app.include_router(metrics.router)
 app.include_router(billing.router)
-=======
-from backend.api import vm, storage
-from backend.ui import router as ui_router
-
-app = FastAPI(title="Cloud Resource Automation Dashboard")
-
-# Register all routers
-app.include_router(vm.router)
-app.include_router(storage.router)
-app.include_router(ui_router)
->>>>>>> 91b5cf815370625b960aa9fdc3934c7152a49015
 
 @app.get("/")
 def read_root():
